7641a43750445a3243f6a80d7387f5039babc22a
JD17VM <jdiegodiego4@gmail.com>
2025-01-10 08:53:51 -0500

    :lipstick: feat(front): add base styles

--- Moliplast-Web-Frontend/src/main.jsx
+++ Moliplast-Web-Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './assets/styles/normalize.css'
+import './assets/styles/estilos_generales.css'
 
 import App from './App.jsx'
 
