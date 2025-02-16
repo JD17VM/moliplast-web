3614d16fae04a2578eb05add9a72eb1fabe997c7
JD17VM <jdiegodiego4@gmail.com>
2025-02-16 17:43:53 -0500

    :bug: fix: pasword hashing in register

--- moliplast-web-backend/app/Http/Controllers/Api/AdministradorController.php
+++ moliplast-web-backend/app/Http/Controllers/Api/AdministradorController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Administrador;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Hash; // Importamos Hash para la verificación
 
 class AdministradorController extends Controller
 {
@@ -17,14 +17,16 @@ class AdministradorController extends Controller
             'nombres' => 'required|string|max:50',
             'apellidos' => 'required|string|max:50',
             'contrasena' => 'required|string|min:8',
-            //'password' => 'required|string|min:8|confirmed', // Cambiado a 'password' y añadido 'confirmed'
         ]);
 
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
 
-        $administrador = Administrador::create($request->all());
+        // No hasheamos aquí, el mutador en el modelo lo hace
+        $data = $request->all();
+
+        $administrador = Administrador::create($data);
 
         return response()->json([
             'message' => 'Administrador creado',
@@ -32,7 +34,6 @@ class AdministradorController extends Controller
             'status' => 201,
         ], 201);
     }
-
 
     public function index()
     {
@@ -57,16 +58,30 @@ class AdministradorController extends Controller
 
         $administrador = Administrador::where('nombres', $request->nombres)->first();
 
-        if (!$administrador || !Hash::check($request->contrasena, $administrador->contrasena)) {
+        if (!$administrador || !Hash::check($request->contrasena, $administrador->contrasena)) { // Usamos Hash::check()
             return response()->json([
+                'lacontra' => $request->contrasena,
+                'tucontra' => $administrador->contrasena,
                 'message' => 'Credenciales incorrectas',
                 'status' => 401
             ], 401);
         }
 
+        // Utilizamos el ID del administrador como token simple
+        $token = $administrador->id;
+
         return response()->json([
             'message' => 'Login exitoso',
             'administrador' => $administrador,
+            'token' => $token,
+            'status' => 200,
+        ], 200);
+    }
+
+    public function perfil(Request $request)
+    {
+        return response()->json([
+            'administrador' => $request->administrador,
             'status' => 200,
         ], 200);
     }
