9afa4578f2a33387ac053db46b010367a7dcc6b4
JD17VM <jdiegodiego4@gmail.com>
2025-02-17 00:55:12 -0500

    :construction: feat: move dataPaginas to Navegador.jsx

--- moliplast-web-frontend/src/widgets/Navegador.jsx
+++ moliplast-web-frontend/src/widgets/Navegador.jsx
@@ -14,7 +14,46 @@ import { Logo_Moliplast } from '../assets/imgs/iconos/svg/Logo_Moliplast';
 
 import { InputButton } from './Form';
 
-const Navegador = ({data}) => {
+
+// Definir dataPaginas directamente en el archivo
+const dataPaginas = {
+    data: [
+        { 
+            nombre: "Inicio", 
+            enlace: "/"
+        },
+        { 
+            nombre: "Nosotros", 
+            enlace: "/nosotros"
+        },
+        { 
+            nombre: "Productos", 
+            enlace: "/productos",
+            subsecciones: [
+                { nombre: "Producto 1", enlace: "/productos/Producto 1" },
+                { nombre: "Producto 2", enlace: "/productos/Producto 2" },
+                { nombre: "Producto 3", enlace: "/productos/Producto 3" },
+            ] 
+        },
+        { 
+            nombre: "Servicios", 
+            enlace: "/servicios",
+        },
+        { 
+            nombre: "Catálogos", 
+            enlace: "/catalogos"
+        },
+        { 
+            nombre: "Contacto", 
+            enlace: "/contacto"
+        }
+    ]
+};
+
+
+const Navegador = () => {
+
+    const data = dataPaginas.data;
 
     const [subseccion_abierta, setSubseccionAbierta] = useState(null);
     const toggleSubseccion = (index) => {
