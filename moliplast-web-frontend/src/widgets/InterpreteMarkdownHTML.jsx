d771d355261c713d0191b3e87d29d274706425b7
JD17VM <jdiegodiego4@gmail.com>
2025-03-05 22:15:02 -0500

    :bug: fix: remove markdown text default in carta producto

--- moliplast-web-frontend/src/widgets/InterpreteMarkdownHTML.jsx
+++ moliplast-web-frontend/src/widgets/InterpreteMarkdownHTML.jsx
@@ -7,33 +7,7 @@ const InterpreteMarkdownHTML = ({ texto_markdown }) => {
     let markdownText = "";
 
     if (!texto_markdown) {
-        markdownText = `
-# Mi tabla con texto
-
-Este es un párrafo de texto antes de la tabla.  Puede incluir **negritas**, *cursivas* y otros elementos Markdown.
-
-| Plugin | README | Descripción |
-|---|---|---|
-| Dropbox | [plugins/dropbox/README.md][PlDb] | Sincroniza archivos con Dropbox. |
-| GitHub | [plugins/github/README.md][PlGh] | Gestiona proyectos en GitHub. |
-| Google Drive | [plugins/googledrive/README.md][PlGd] | Almacena y comparte archivos en Google Drive. |
-| OneDrive | [plugins/onedrive/README.md][PlOd] | Sincroniza archivos con OneDrive. |
-| Medium | [plugins/medium/README.md][PlMe] | Publica artículos en Medium. |
-| Google Analytics | [plugins/googleanalytics/README.md][PlGa] | Analiza el tráfico de tu sitio web. Analiza el tráfico de tu sitio web. Analiza el tráfico de tu sitio web. |
-
-Este es otro *párrafo* de **texto** después de la tabla.  También puede incluir enlaces como [este](https://www.ejemplo.com).
-
-- Esta esla opcion 1
-- Esta esla opcion 1
-- Esta esla opcion 1
-- Esta esla opcion 1
-- Esta esla opcion 1
-
-
-# Más texto
-
-Incluso puedes tener más encabezados y contenido Markdown después de la tabla.
-        `;
+        markdownText = ``;
     } else {
         markdownText = texto_markdown;
     }
