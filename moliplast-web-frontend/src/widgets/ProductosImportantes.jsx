d99e84febe947a585f505ca59fe06751ced48f87
JD17VM <jdiegodiego4@gmail.com>
2025-03-07 09:19:55 -0500

    :construction: feat: add ProductosImportantes data prop

--- moliplast-web-frontend/src/widgets/ProductosImportantes.jsx
+++ moliplast-web-frontend/src/widgets/ProductosImportantes.jsx
@@ -3,20 +3,13 @@ import CartaProducto from './CartaProducto';
 import imageHelper from '../utils/imageHelper.js'
 import { ContenedorSeccion } from '../Inicio';
 
-// Función para construir la URL completa de la imagen
-const getFullImageUrl = (path) => {
-    if (!path) return '';
-    return path.startsWith('http') ? path : `${BASE_URL_API}${path}`;
-};  
-
-export const SeccionProductosImportantes = ({titulo = "Productos Importantes"}) => {
+export const SeccionProductosImportantes = ({titulo = "Productos Importantes", data}) => {
     return(
         <ContenedorSeccion titulo={titulo} color_fondo="negro">
             <div className={styles.contenedor_productos_destacados} data-aos="fade-up">
-                <CartaProducto enlace_imagen={imageHelper.ImagenDemoProducto1} texto="ELECTROVANNE NAANDAN 2" />
-                <CartaProducto enlace_imagen={imageHelper.ImagenDemoProducto2} texto="ELECTROBOMBA SUMERGIBLE FORAS" />
-                <CartaProducto enlace_imagen={imageHelper.ImagenDemoProducto3} texto="Biodigestor Autolimpiable 1300 Litros" />
-                <CartaProducto enlace_imagen={imageHelper.ImagenDemoProducto4} texto="GREENRAIN SYSTEM | VALVULA ELECTRICA SERIE DVF LINEAL 1” RH" />
+            {data.map((producto, index) => (
+                <CartaProducto key={index} enlace_imagen={producto.img || imageHelper.defaultImg} texto={producto.nombre} id={producto.id}/>
+            ))} 
             </div>
         </ContenedorSeccion>
     )
