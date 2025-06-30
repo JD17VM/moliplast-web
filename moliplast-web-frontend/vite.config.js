7a79918c741a538b4bd146d5ac2caba46238257c
JD17VM <jdiegodiego4@gmail.com>
2025-06-30 01:19:57 -0500

    :package: build: add @vitejs/plugin-basic-ssl as a dev dependency

--- moliplast-web-frontend/vite.config.js
+++ moliplast-web-frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    basicSsl() // 2. Añade el plugin
+  ],
+  server: {
+    host: true, // Esto hace que el servidor escuche en todas las interfaces de red disponibles
+    port: 5173, // Puedes especificar un puerto diferente si el 5173 ya está en uso
+  },
 })
